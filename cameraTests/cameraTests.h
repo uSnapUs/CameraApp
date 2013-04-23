f392e39d8b74047a5ae11cb9c1916c2d42a5214a
Owen Evans <owen@bgeek.net>
2013-04-24 10:10:06 +1200

    Initial Commit

---
+++ cameraTests/cameraTests.h
@@ -0,0 +1,13 @@
+//
+//  cameraTests.h
+//  cameraTests
+//
+//  Created by Owen Evans on 24/04/13.
+//  Copyright (c) 2013 Owen Evans. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface cameraTests : SenTestCase
+
+@end
---
+++ cameraTests/cameraTests.m
@@ -0,0 +1,32 @@
+//
+//  cameraTests.m
+//  cameraTests
+//
+//  Created by Owen Evans on 24/04/13.
+//  Copyright (c) 2013 Owen Evans. All rights reserved.
+//
+
+#import "cameraTests.h"
+
+@implementation cameraTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in cameraTests");
+}
+
+@end
